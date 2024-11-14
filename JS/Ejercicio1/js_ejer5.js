afba841da33a995821e8012e63a6ff8d6b8d27db
CLHPD <alepando121@gmail.com>
2024-11-14 14:16:49 +0100

    Solo JS

---
+++ JS/Ejercicio1/js_ejer5.js
@@ -0,0 +1,7 @@
+var nombre;
+var apellido1;
+var apellido2;
+nombre = prompt("Escriba su nombre:");
+apellido1 = prompt("Escriba su primer apellido");
+apellido2 = prompt("Escriba su segundo apellido");
+document.write(nombre  +  apellido1  + apellido2);
--- JS/js_ejer1.js
+++ JS/js_ejer1.js
@@ -1,3 +1,12 @@
-document.writeln("Alejandro" + "<br>");
-document.writeln("Guerrero" + "<br>");
-document.writeln("SMR" + "<br>");
+var numero;
+numero = prompt("Escriba un numero");
+
+if (isNaN(numero)){
+    document.writeln("Porfavor intrdoduzca un numero");
+}
+else if ((numero % 2) == 0){
+    document.writeln("El numero es par");
+}
+else{
+    document.writeln("El numero es impar");
+}
